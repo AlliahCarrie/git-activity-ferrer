1cf40e0f37f213a9d964640c32ebfb92e0f6a618
Alliah Carrie Ferrer <ferreralliah863@gmail.comgit config --global user.email ferreralliah863@gmail.com>
2024-07-18 13:40:09 +0800

    branch

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<h1> kiwiawidiwi <h1>
-<h2> Riririri <h2>
-<h3> newnew <h3>
-<h4> updatenew <h4>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Alliah Carrie A. Ferrer </h1>
+    <h2> 33-ITE-03 </h2>
+    <h3> Summer </h3>
+</body>
+</html>
